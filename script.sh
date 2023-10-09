a658b6049346e3f2ae09b892d1609ef8e658db6c
surya-ai <66955399+surya-ai@users.noreply.github.com>
2023-10-09 07:46:53 +0000

    new

--- script.sh
+++ script.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-sudo su;
 sudo apt update -y && sudo apt upgrade -y;
 sudo apt install xfce4 tigervnc-standalone-server firefox-esr qterminal dbus-x11 -y;
 sudo wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz;
 sudo tar -xvf ngrok-v3-stable-linux-amd64.tgz;
 sudo rm ngrok-v3-stable-linux-amd64.tgz;
 sudo mv ngrok /usr/local/bin;
-sudo echo "vncserver :1;\n ngrok tcp 5901;" >>/usr/local/bin/vnc;
-sudo echo "vncserver -kill :*" >>/usr/local/bin/vs;
-sudo chmod +x /usr/local/bin/vs;
-sudo chmod +x /usr/local/bin/vnc;
 ngrok config add-authtoken 2LbHfwg2lKxJhj1dqwQV8UZN93N_36vRKYuAMAoVPiooZ3efS;
 cd $HOME;
 sudo echo "sudo apt-get install wget gpg
@@ -20,4 +15,8 @@ rm -f packages.microsoft.gpg" >> s.sh;
 sudo bash s.sh;
 sudo apt install apt-transport-https;
 sudo apt update;
-sudo apt install code;
+sudo apt install code -y;
+sudo su -c 'echo "vncserver :1; ngrok tcp 5901" >>/usr/local/bin/vnc; exit;';
+sudo su -c 'echo "vncserver -kill :*" >>/usr/local/bin/vs; exit;';
+sudo chmod +x /usr/local/bin/vs;
+sudo chmod +x /usr/local/bin/vnc;

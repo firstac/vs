14aa7cdbc9ffb67d8ace0093a50846a6cd37de73
surya-ai <66955399+surya-ai@users.noreply.github.com>
2023-10-12 10:23:01 +0545

    new

--- projects/index2.php
+++ projects/index2.php
@@ -1,22 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Local Timezone</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
 </head>
+
 <body>
-    <div id="userTimezone"></div>
-    <div id="serverTimezone"></div>
 
-    <script>
-        // JavaScript code to get the user's local timezone
-        const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        document.getElementById("userTimezone").textContent = `Your local timezone is: ${userTimeZone}`;
-    </script>
-
-    <?php
-     // Set your server's timezone, e.g., UTC
-    $serverTimeZone = date_default_timezone_get(); // Get the server's timezone
-    echo "<script>document.getElementById('serverTimezone').textContent = 'Server timezone is: $serverTimeZone';</script>";
-    ?>
 </body>
+
 </html>
